318251d215e759c0a47b76dd1920447cb3bce699
Variya Dhruv <dhruvvariya2003@gmail.com>
2024-05-01 22:42:08 +0530

    commit 1

--- MainService/Program.cs
+++ MainService/Program.cs
@@ -1,15 +1,29 @@
+using MainService.Data;
+using MainService.Services.movieManagementService;
+using MainService.Services.SeriesService;
+using MainService.Services.UserService;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 using System.Text;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add database context and connection string to migrate database using EfCore
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder
+    .Configuration.GetConnectionString("DefaultConnection")));
 
+// Add services to the container.
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+// automapper
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+//Add Service to program.cs
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IMovieManagementService, movieManagementService>();
+builder.Services.AddScoped<ISeriesService, SeriesService>();
 
 builder.Services.AddSwaggerGen(options =>
 {
@@ -36,23 +50,29 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
+
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseCors(options =>
 {
     options.AllowAnyHeader();
     options.AllowAnyMethod();
-    options.AllowAnyOrigin();
+    options.AllowAnyOrigin();   
 });
+
+app.UseStaticFiles();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
