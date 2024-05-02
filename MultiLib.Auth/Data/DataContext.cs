538b79c96802b9f1232d4e15972d76459a9eaf6c
DhruvVariya <dhruv.variya@samudra.co.in>
2024-05-02 16:35:46 +0530

    final

---
+++ MultiLib.Auth/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiLib.Auth.models;
+
+namespace MultiLib.Auth.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        
+        public DbSet<User> Users => Set<User>();
+        
+
+    }
+}
---
+++ MultiLib.Auth/Services/UserService/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using MultiLib.Auth.Dtos.User;
+using MultiLib.Auth.models;
+
+
+namespace MultiLib.Auth.Services.UserService
+{
+    public interface IUserService
+    {
+        Task<ServiceResponse<AuthUserDto>> Authenticate(string username, string password);
+        Task<ServiceResponse<List<GetUserDto>>> GetAllUsers();
+        Task<ServiceResponse<List<RegisterUserDto>>> RegisterUser(RegisterUserDto newUser);
+
+    }
+}
